e59f19d887ad7fe9d63dfc6b2e99e4d8e441ea7d
Untraditional <58677398+Untraditional@users.noreply.github.com>
2023-04-12 17:23:03 -0400

    Completed testMixed and testDupe
    Fixed both the testMixed and testDuplicate Junit functions.

--- Lab10/src/testSelectionSort.java
+++ Lab10/src/testSelectionSort.java
@@ -46,13 +46,43 @@ public class testSelectionSort {
     }
     
     public void testMixed(){
+    	int[] arr = new int[5];
+        arr[0] = -8;
+        arr[1] = 9;
+        arr[2] = -7;
+        arr[3] = 10;
+        arr[4] = -2;
+        
+        int[] Sortedarr = new int[5];
+        Sortedarr[0] = -8;
+        Sortedarr[1] = -7;
+        Sortedarr[2] = -2;
+        Sortedarr[3] = 9;
+        Sortedarr[4] = 10;
         
         /** Test data contains with both positive, negative and zeros **/
+        SelectionSort test = new SelectionSort();
+        assertArrayEquals(Sortedarr, test.basicSelectionSort(arr));
     }
     
     public void testDuplicates(){
+    	int[] arr = new int[5];
+        arr[0] = 1;
+        arr[1] = 1;
+        arr[2] = 7;
+        arr[3] = 10;
+        arr[4] = 9;
+        
+        int[] Sortedarr = new int[5];
+        Sortedarr[0] = 1;
+        Sortedarr[1] = 1;
+        Sortedarr[2] = 7;
+        Sortedarr[3] = 9;
+        Sortedarr[4] = 10;
         
         /** Test data contains duplicates **/
+        SelectionSort test = new SelectionSort();
+        assertArrayEquals(Sortedarr, test.basicSelectionSort(arr));
     }
 
 
